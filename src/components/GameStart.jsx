49e089de6a308a3c73aa9dcd4ab3177944430147
SimonasGer <159446055+SimonasGer@users.noreply.github.com>
2025-07-14 19:17:43 +0300

    0.01

--- src/components/GameStart.jsx
+++ src/components/GameStart.jsx
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom"
 export const GameStart = () => {
     return (
         <div className="game-start">
             <h1>Welcome to Golden Scorpion!</h1>
             <p>Get ready to embark on an exciting adventure.</p>
-            <button className="start-button">Start Game</button>
+            <Link to="/game" className="start-button">Start Game</Link>
         </div>
     )
 }
