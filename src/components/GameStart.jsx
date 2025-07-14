abc2ca8ff251f5eb00b72ae1682ef30f6417a974
SimonasGer <159446055+SimonasGer@users.noreply.github.com>
2025-07-14 19:14:28 +0300

    first commit

---
+++ src/components/GameStart.jsx
@@ -0,0 +1,9 @@
+export const GameStart = () => {
+    return (
+        <div className="game-start">
+            <h1>Welcome to Golden Scorpion!</h1>
+            <p>Get ready to embark on an exciting adventure.</p>
+            <button className="start-button">Start Game</button>
+        </div>
+    )
+}
