804c65fbf2723ba6beb928d96a86ecdcaf79c5cf
unknown <simonasgerulis1@gmail.com>
2025-07-20 09:48:05 +0300

    fixed token exploit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import jwtDecode from "jwt-decode";
 import { GameStart } from "./components/TitleScreen/GameStart";
 import { MainMenu } from "./components/Game/MainMenu";
 import { Login } from "./components/TitleScreen/auth/Login";
 import { Register } from "./components/TitleScreen/auth/Register";
 
+const isTokenValid = (token) => {
+  try {
+    const decoded = jwtDecode(token)
+    const now = Date.now() / 1000
+    const valid = decoded.exp && decoded.exp > now
+    if (!valid) {
+      localStorage.removeItem("token")
+    }
+    return valid
+  } catch (e) {
+    localStorage.removeItem("token")
+    return false
+  }
+}
+
+
 const ProtectedRoute = ({ children }) => {
     const token = localStorage.getItem("token");
-    return token ? children : <Navigate to="/" />;
+    return token && isTokenValid(token) ? children : <Navigate to="/"/>;
 }
 
 const PublicRoute = ({ children }) => {
   const token = localStorage.getItem("token");
-  return token ? <Navigate to="/game" /> : children;
+  return token && isTokenValid(token) ? <Navigate to="/game"/> : children;
 }
 
 function App() {
